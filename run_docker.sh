f8f22f9d69d10e0b871980f423b8ba5f19fd83d7
soeren <2378192-soerenschneider@users.noreply.gitlab.com>
2020-08-02 20:45:13 +0200

    Removed unneeded files

--- run_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-docker build -t immospider .
-docker run -d --name immospider --env-file ./config immospider
-# docker logs immospider
-# docker cp df134a4abcc8:/app/apartments.csv docker_apartments.csv
